29d3cbd78b45f00a4a771a688564135fc4b78140
LeticiaBarbosa-dev <leticiapereiraab@gmail.com>
2021-07-16 20:55:28 -0300

    Quarta versão - corrigindo o bug de não salvar no localStorage

--- js/script.js
+++ js/script.js
@@ -1,42 +1,27 @@
 const form = document.getElementById('form');
-const messageEmail = document.querySelector('#message-email');
-const messageName = document.querySelector('#message-nome');
-const message = document.querySelector('#message-alert');
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const nome = document.getElementById('nome');
-    const email = document.getElementById('email');
-
-    if(nome.value != '' && email.value != ''){
-        const data = {
-            nome,
-            email,
-        }
-        let convertData = JSON.stringify(data);
-        localStorage.setItem('storage', convertData);
-        setTimeout(() =>{
-            message.innerHTML = "Cadastro realizado com sucesso!";
-            nome.value = "";
-            email.value = "";
-        }, 2000);
-        
-    }else{
-        if(nome.value == '' && email.value == ''){
-            message.innerHTML = 'Preencha os campos acima!';
-            setTimeout(() => {
-                message.innerHTML = '';
-            }, 900);
-        }else if(email.value == ''){
-            messageEmail.innerHTML = '*Email obrigatório!';
-            setTimeout(() => {
-                messageEmail.innerHTML = '';
-            }, 900);
-        }else if(nome.value == '') {
-            messageName.innerHTML = '*Nome obrigatório!';
-            setTimeout(() => {
-                messageName.innerHTML = '';
-            }, 900);
-        }
+form.addEventListener('submit',(e) =>{
+    e.preventDefault()
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let data = {
+        nome,
+        email,
     }
+    let convertData = JSON.stringify(data);
+
+    localStorage.setItem('lead', convertData);
+
+    let content = document.getElementById('form');
+
+    let done =  `<h2 id="success" style="color: white;">CADASTRO REALIZADO COM SUCESSO!💪</h2>`
+
+    let message = document.getElementById('mensagem');
+
+    let clean = `<p></p>`
+
+    setTimeout(() =>{
+        message.innerHTML = clean;
+        content.innerHTML = done;
+    },2000)
 })
